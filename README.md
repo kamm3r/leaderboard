d71c04356b21bfcdc95907dc2f7e2c82d10b003e
Marco <marcokammer59@gmail.com>
2023-01-24 15:18:34 +0200

    added Board component n more

--- README.md
+++ README.md
@@ -18,26 +18,29 @@ pnpm dev
 - [ ] add nextauth so that multiple user can use it
 - [x] data revalidatetion
 - [ ] athlete features
-  - [x] add a athlete and it show up on the dashboard
-  - [x] remove an athlete
-  - [x] clear all athletes
-  - [ ] sorting
-    - [x] sort by
-    - [ ] reset sort by
-    - [ ] persitent sorting
-  - [ ] re-order athletes with use gesture??
-  - [ ] transition to when added new athlete
+        - [x] add a athlete and it show up on the dashboard
+        - [x] remove an athlete
+        - [x] clear all athletes
+        - [ ] sorting
+          - [x] sort by
+          - [ ] undo to og order
+          - [ ] persitent sorting
+          - sorting the best to worst, reverse and undo to og order (with quicksort)
+        - [ ] re-order athletes with use gesture??
+        - [ ] transition to when added new athlete
 - [ ] Attempt features
-  - [x] add a attempt
-  - [x] fix add a attempt by getting the right athlete id
-    - fix to show only clicked athlete attempt to add show, not all stack
-  - [ ] adding a attempt status indicator
-  - [ ] double-click to edit a attempt
+        - [x] add a attempt
+        - [ ] adding a attempt status indicator
+        - [ ] adding a athletes result state 3 dot animated???
+        - [ ] double-click to edit a attempt??
 - [ ] timetable features
-  - [ ] add scheduled times
-  - [ ] edit timetable
+        - [ ] add scheduled times
+        - [ ] edit timetable
+        - [ ] timetable column redesign
 - [ ] add another language options
 - [ ] able to change meet status final or whatever
 - [ ] able to change meet name
-- [ ] tabs for men's and women's meets??
+  - [ ] add competion name to db??
+- [ ] tabs for men's and women's meets?? tabs for male n female athletes list
+- [ ] show leaderboard with current athletes
 - think about a better name to this project
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import clsx from "clsx";
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import {
@@ -19,72 +18,60 @@ import {
 import * as portals from "react-reverse-portal";
 import { useTRPCForm } from "trpc-form";
 
-import LoadingSVG from "../assets/puff.svg";
 import { AutoAnimate } from "../components/auto-animate";
 import Button from "../components/button";
 import { Card } from "../components/card";
 import { TextInput } from "../components/text-input";
 import { addAttemptInput } from "../shared/add-athlete-validator";
-import { api } from "../utils/api";
+import { type RouterOutputs, api } from "../utils/api";
+import { quicksort } from "../utils/quicksort";
 
-/* add better typing then just any L man for that
- fix this real quick */
-function AddAttempt({
-  onClose,
-  athleteId,
-}: {
+const AddAttempt: React.FC<{
   onClose: () => void;
   athleteId?: string;
-}) {
-  // const { refetch } = api.athletes.getAll.useQuery();
-  // const { mutate, isLoading } = api.athletes.addAttempt.useMutation({
-  //   onSuccess: (data) => {
-  //     console.log("submitted", data);
-  //     setModal(false);
-  //     void refetch();
-  //   },
-  // });
-  // const {
-  //   handleSubmit,
-  //   register,
-  //   formState: { errors },
-  // } = useForm<AddAttemptInputType>({
-  //   resolver: zodResolver(addAttemptInput),
-  // });
-
+}> = ({ onClose, athleteId }) => {
   const utils = api.useContext();
 
   const form = useTRPCForm({
     mutation: api.athletes.addAttempt,
     validator: addAttemptInput,
-
     onSuccess: () => {
       void utils.athletes.invalidate();
-      setModal(false);
-      // void refetch();
+      onClose();
     },
-    onSubmit: async (input) => {
-      utils.athletes.getAthlete.setData(
-        input.data.athleteId,
-        input.data.attempt1
-      );
+    onSubmit: async () => {
       await new Promise((r) => setTimeout(r, 1000));
     },
   });
 
   return (
-    <div className="absolute top-0 right-0 left-0 bottom-0 z-50 flex h-screen w-screen items-center justify-center bg-black/50">
-      <div className="relative rounded bg-neutral-800 p-5">
-        <button className="absolute top-5 right-5" onClick={onClose}>
+    <dialog
+      onClick={onClose}
+      className="absolute top-0 right-0 left-0 bottom-0 z-50 flex h-screen w-screen items-center justify-center bg-black/50"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="relative rounded bg-neutral-800 p-5"
+      >
+        <button onClick={onClose} className="absolute top-5 right-5">
           L + Ratio
         </button>
-        <form
-          className="flex flex-col gap-5"
-          // onSubmit={handleSubmit((data) =>
-          //   mutate({ athleteId, attempt1: data.attempt1 })
-          // )}
-          ref={form.ref}
-        >
+        <form ref={form.ref} className="flex flex-col gap-5">
+          {/* TODO: Remove it or change it to a name??? */}
+          <div className="flex flex-col gap-2">
+            <label className="font-normal" htmlFor={form.athleteId?.id()}>
+              Athlete ID
+            </label>
+            <input
+              className="mt-1 rounded bg-neutral-900/50 py-2 px-3 focus:border-neutral-500 focus:ring-neutral-500 sm:text-sm"
+              type="text"
+              name={form.athleteId?.name()}
+              value={athleteId}
+            />
+            {form.athleteId?.error((e) => (
+              <p className="py-2 text-xs text-red-400">{e.message}</p>
+            ))}
+          </div>
           <div className="flex flex-col gap-2">
             <label className="font-normal" htmlFor={form.attempt1.id()}>
               Add attempt
@@ -92,7 +79,6 @@ function AddAttempt({
             <input
               className="mt-1 rounded bg-neutral-900/50 py-2 px-3 focus:border-neutral-500 focus:ring-neutral-500 sm:text-sm"
               type="text"
-              // {...register("attempt1")}
               name={form.attempt1.name()}
               placeholder="60.69"
             />
@@ -111,9 +97,74 @@ function AddAttempt({
           </Button>
         </form>
       </div>
+    </dialog>
+  );
+};
+
+type StatusProp = "Qualification" | "Semi-Final" | "Final";
+
+const Board: React.FC<{
+  data: RouterOutputs["athletes"]["getAll"];
+  status: StatusProp;
+  meetName: string;
+  onShow: boolean;
+}> = ({ data, status, meetName, onShow }) => {
+  return (
+    <div className="max-w-xs">
+      <div className="w-full max-w-xs border-t-2 border-cyan-300 bg-black/90">
+        <h1 className="px-2 uppercase text-cyan-300">{status}</h1>
+        <div className="flex">
+          <h3 className="bg-cyan-300 px-2 uppercase text-black">
+            Men&#39;s hammer throw
+          </h3>
+          <h4 className="px-2 uppercase">Distance</h4>
+        </div>
+        <ul className="flex flex-col">
+          {data.map((a) => (
+            <li
+              key={a.id}
+              className=" flex flex-wrap justify-between border-t-2 border-black/50"
+            >
+              <div className="flex flex-[1_1_100%] justify-between px-4 py-1">
+                {a.name}
+                <span>75.10m</span>
+              </div>
+              <ul
+                className={clsx(
+                  "ml-1  flex-[1_1_100%] bg-gray-300 text-black",
+                  onShow ? "flex" : "hidden"
+                )}
+              >
+                {/* bg-cyan-300/50 is for highest value */}
+                <li className="min-w-[16.7%] px-1 py-3">
+                  {a.attempts[0]?.attempt1}
+                </li>
+                <li className="min-w-[16.7%] bg-gray-200 px-1 py-3">
+                  {a.attempts[1]?.attempt1}
+                </li>
+                <li className="min-w-[16.7%] bg-cyan-300/50 px-1 py-3">
+                  {a.attempts[2]?.attempt1}
+                </li>
+                <li className="min-w-[16.6%] px-1 py-3">
+                  {a.attempts[3]?.attempt1}
+                </li>
+                <li className="min-w-[16.7%] bg-gray-200 px-1 py-3">
+                  {a.attempts[4]?.attempt1}
+                </li>
+                <li className="min-w-[16.7%] px-1 py-3">
+                  {a.attempts[5]?.attempt1}
+                </li>
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <h1 className="mt-1 inline-flex bg-black/90 p-1 uppercase text-cyan-300">
+        {meetName}
+      </h1>
     </div>
   );
-}
+};
 
 const AthleteView = () => {
   const portalNode = React.useMemo(() => {
@@ -126,10 +177,7 @@ const AthleteView = () => {
   const [but, setBut] = useState(false);
   const [modal, setModal] = useState(false);
   const [id, setId] = useState<string>();
-  // const toggle = (id: string) => {
-  //   setModal((modal) => !modal);
-  //   setId(id);
-  // };
+  const [meetName, setMeetName] = useState("");
   const toggle = React.useCallback(
     (id: string) => {
       setModal(!modal);
@@ -155,14 +203,95 @@ const AthleteView = () => {
 
   if (isLoading)
     return (
-      <div className="animate-fade-in-delay flex justify-center p-8">
-        {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
-        <Image src={LoadingSVG} alt="loading..." width={200} height={200} />
+      <div className="grid min-h-0 flex-1 grid-rows-3 gap-4 p-4 sm:grid-cols-7 sm:grid-rows-1 sm:gap-8 sm:p-8">
+        {/* timetable */}
+        <div className="col-span-1 row-span-1 flex flex-col gap-4">
+          <div className="flex items-center justify-between">
+            <h2 className="flex items-center gap-1.5 font-medium" />
+          </div>
+
+          <AutoAnimate className="flex flex-col gap-4">
+            <Card className="relative flex animate-pulse flex-col gap-4 p-4">
+              <div className="break-words" />
+              <div className="break-words" />
+              <div className="flex items-center justify-between text-gray-300">
+                <button />
+                <button className="relative z-10 -my-1 -mx-2 flex items-center gap-1.5 rounded py-1 px-2 text-sm hover:bg-neutral-900/50 hover:text-white" />
+              </div>
+            </Card>
+            <Card className="relative flex animate-pulse flex-col gap-4 p-4">
+              <Button size="lg" variant="text" />
+              <Button size="lg" variant="text" />
+              <Button size="lg" variant="text" />
+              <Button size="lg" variant="text" />
+            </Card>
+            <Card className="relative flex animate-pulse flex-col gap-4 p-4" />
+          </AutoAnimate>
+        </div>
+        {/* Preview window */}
+        <div className="row-span-1 flex sm:col-span-4">
+          <Card className="flex flex-1 animate-pulse flex-col divide-y divide-neutral-900">
+            <div className="flex min-h-0 flex-1 flex-col p-4">
+              <div className="flex min-h-0 flex-1 flex-col">
+                <div className="flex items-baseline justify-between">
+                  <h2 className="font-bold" />
+                  <Button className="-m-2 !p-2" variant="ghost">
+                    <div className="flex items-center" />
+                  </Button>
+                </div>
+                <div className="min-h-0 flex-1 overflow-y-auto">
+                  <AutoAnimate className="flex min-h-full items-center justify-center" />
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-3 divide-x  divide-neutral-900">
+              <button className="flex items-center justify-center gap-2 rounded-br p-3 text-sm hover:bg-neutral-700 sm:p-4 sm:text-base" />
+              <button className="flex items-center justify-center gap-2 rounded-br p-3 text-sm hover:bg-neutral-700 sm:p-4 sm:text-base" />
+
+              <button className="flex items-center justify-center gap-2 rounded-br p-3 text-sm hover:bg-neutral-700 sm:p-4 sm:text-base" />
+            </div>
+          </Card>
+        </div>
+        {/* Athletes column */}
+        <div className="row-span-2 flex flex-col gap-2 sm:col-span-2 sm:row-span-1">
+          <div className="flex items-center justify-between">
+            <h2 className="flex items-center gap-1.5 font-medium">
+              <span />
+              <span className="inline-flex h-6 w-6 items-center justify-center rounded-full bg-neutral-800 text-xs font-extrabold" />
+              <button className="relative z-10 -my-2 flex items-center gap-1.5 rounded py-2 px-2  hover:bg-neutral-900/50 hover:text-white" />
+            </h2>
+          </div>
+          <AutoAnimate className="flex min-h-0 flex-1 flex-col rounded-lg bg-neutral-800/25">
+            <AutoAnimate
+              as="ul"
+              className="flex flex-col gap-2 overflow-y-auto p-2"
+            >
+              <li>
+                <Card className="relative flex animate-pulse flex-col gap-4 p-4">
+                  <div className="flex items-center gap-3 break-words">
+                    <p className="" />
+                    <span className="text-xs opacity-70" />
+                  </div>
+
+                  <div className="flex gap-2">
+                    <p className="-my-1 rounded bg-neutral-900/50 px-2 py-1 text-sm" />
+                    <button className="relative -my-1 flex items-center gap-1.5 rounded px-2 py-1 text-sm" />
+                  </div>
+                  <div className="flex items-center justify-between text-gray-300">
+                    <button className="relative z-10 -my-1 -mx-2 flex items-center gap-1.5 rounded py-1 px-2 text-sm" />
+                    <button className="relative z-10 -my-1 -mx-2 flex items-center gap-1.5 rounded py-1 px-2 text-sm" />
+                  </div>
+                </Card>
+              </li>
+            </AutoAnimate>
+          </AutoAnimate>
+        </div>
       </div>
     );
 
   const Sorted = () => {
     if (reverseSort) {
+      console.log("pass");
       data?.sort((a, b) => {
         const BValue = b.attempts.map((at) => parseFloat(at.attempt1));
         const AValue = a.attempts.map((at) => parseFloat(at.attempt1));
@@ -170,14 +299,51 @@ const AthleteView = () => {
       });
       setReverseSort(!reverseSort);
     } else {
-      data?.sort((a, b) => {
-        const BValue = b.attempts.map((at) => parseFloat(at.attempt1));
-        const AValue = a.attempts.map((at) => parseFloat(at.attempt1));
-        return Math.max.apply(null, BValue) - Math.max.apply(null, AValue);
-      });
+      console.log("smash");
+      data
+        ?.sort((a, b) => {
+          const BValue = b.attempts.map((at) => parseFloat(at.attempt1));
+          const AValue = a.attempts.map((at) => parseFloat(at.attempt1));
+          return Math.max.apply(null, AValue) - Math.max.apply(null, BValue);
+        })
+        .reverse();
       setReverseSort(!reverseSort);
     }
   };
+  // const Sorted = () => {
+  //   if (reverseSort) {
+  //     data?.sort((a, b) => {
+  //       const BValue = b.attempts.map((at) => parseFloat(at.attempt1));
+  //       const AValue = a.attempts.map((at) => parseFloat(at.attempt1));
+  //       return Math.max.apply(null, AValue) - Math.max.apply(null, BValue);
+  //     });
+  //     setReverseSort(!reverseSort);
+  //   } else {
+  //     data?.sort((a, b) => {
+  //       const BValue = b.attempts.map((at) => parseFloat(at.attempt1));
+  //       const AValue = a.attempts.map((at) => parseFloat(at.attempt1));
+  //       return Math.max.apply(null, BValue) - Math.max.apply(null, AValue);
+  //     });
+  //     setReverseSort(!reverseSort);
+  //   }
+  // };
+
+  // const otherAthlete = data;
+
+  // const athletesSorted = reverseSort
+  //   ? [...otherAthlete].reverse()
+  //   : otherAthlete;
+
+  const ass = data?.map((a) => a.attempts);
+
+  const ssd = ass?.map((a) => a.map((at) => at.attempt1));
+  const s = ssd?.map((as) => as.map(parseFloat));
+  const ff: any[] = new Array(s?.flat());
+
+  console.log("BOBSBefore", ff);
+  quicksort(ff, 0, ff.length - 1);
+  console.log("BOBSorVagana", ssd);
+  console.log("BOBS", ff);
 
   return (
     <>
@@ -270,6 +436,8 @@ const AthleteView = () => {
               type="text"
               prefixEl="Competition"
               maxLength={400}
+              value={meetName}
+              onChange={(e) => setMeetName(e.target.value)}
             />
             <div className="flex min-h-0 flex-1 flex-col p-4">
               <div className="flex min-h-0 flex-1 flex-col">
@@ -364,13 +532,23 @@ const AthleteView = () => {
                           </ul>
                         </div>
                         <h1 className="mt-1 inline-flex bg-black/90 p-1 uppercase text-cyan-300">
-                          Tour De Hammer 2023
+                          {meetName}
                         </h1>
                       </div>
                     ) : (
-                      <p className="text-sm font-medium text-gray-600">
+                      <>
+                        {/* <p className="text-sm font-medium text-gray-600">
                         No active tablo
-                      </p>
+                       </p> */}
+                        {data && (
+                          <Board
+                            data={data}
+                            status={"Final"}
+                            meetName={meetName}
+                            onShow={show}
+                          />
+                        )}
+                      </>
                     )}
                   </AutoAnimate>
                 </div>
@@ -392,11 +570,7 @@ const AthleteView = () => {
                   </>
                 )}
               </button>
-              <button
-                className="flex items-center justify-center gap-2 rounded-br p-3 text-sm hover:bg-neutral-700 sm:p-4 sm:text-base"
-                // onClick={() => unpinAllAthlete()}
-              >
-                {/* <FaEyeSlash /> */}
+              <button className="flex items-center justify-center gap-2 rounded-br p-3 text-sm hover:bg-neutral-700 sm:p-4 sm:text-base">
                 {/* TODO maybe fix */}
                 **All Athlete Results**
               </button>
@@ -405,7 +579,6 @@ const AthleteView = () => {
                 className="flex items-center justify-center gap-2 rounded-br p-3 text-sm hover:bg-neutral-700 sm:p-4 sm:text-base"
                 onClick={() => setShow(!show)}
               >
-                {/* <FaCaretSquareRight /> */}
                 Next Athlete
               </button>
             </div>
@@ -435,10 +608,7 @@ const AthleteView = () => {
               {data?.map((ath) => (
                 <li key={ath.id}>
                   <Card className="animate-fade-in-down relative flex flex-col gap-4 p-4">
-                    <div
-                      key={ath.id}
-                      className="flex items-center gap-3 break-words"
-                    >
+                    <div className="flex items-center gap-3 break-words">
                       <p className="">{ath.name}</p>
                       <span className="text-xs opacity-70">
                         PB: {ath.PB} SB: {ath.SB}
