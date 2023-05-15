0ce0f80f291d946199fad12ef56584c30bc39d93
Marco <marcokammer59@gmail.com>
2023-05-15 15:02:12 +0300

    login works now n updated ci

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,19 +1,16 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-import { env } from "~/env.mjs";
-import { createTRPCContext } from "~/server/api/trpc";
-import { appRouter } from "~/server/api/root";
+import { env } from "../../../env.mjs";
+import { createTRPCContext } from "../../../server/api/trpc";
+import { appRouter } from "../../../server/api/root";
 
 // export API handler
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-expect-error
 export default createNextApiHandler({
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   router: appRouter,
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   createContext: createTRPCContext,
   onError:
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
