89baa3be6e4e84036fdeab5c94d7262bf4f99d3e
Marco <marcokammer59@gmail.com>
2023-05-15 10:36:01 +0300

    ignored/fixes some errors

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -5,10 +5,15 @@ import { createTRPCContext } from "~/server/api/trpc";
 import { appRouter } from "~/server/api/root";
 
 // export API handler
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error
 export default createNextApiHandler({
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   router: appRouter,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   createContext: createTRPCContext,
   onError:
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
